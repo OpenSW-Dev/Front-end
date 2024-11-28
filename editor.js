b186d8333e2dd6539441474b5875b766fa627e4b
sudokube <@>
2024-11-28 20:48:16 +0900

    Add functionality to dynamically add & remove ingredients

--- editor.js
+++ editor.js
@@ -7,7 +7,44 @@ document.addEventListener("DOMContentLoaded", function () {
         ["image", "code-block"],
       ],
     },
-    placeholder: "Compose an epic...",
+    placeholder: "Compose an epic recipe...",
     theme: "snow", // or 'bubble'
   });
 });
+
+function removeIngredient(button) {
+  const ingredientContainer = button.parentElement;
+  ingredientContainer.remove();
+}
+
+const ingredientsList = document.getElementById("ingredients-list");
+const ingredientButton = document.getElementById("ingredient-btn");
+
+ingredientButton.onclick = () => {
+  const ingredientContainer = document.createElement("div");
+  ingredientContainer.className = "ingredient-container";
+
+  const ingredient = document.createElement("input");
+  ingredient.className = "ingredient";
+  ingredient.placeholder = "Ingredient";
+  ingredient.text = "text";
+
+  const removeButton = document.createElement("button");
+  removeButton.className = "btn remove-btn";
+  removeButton.textContent = "-";
+
+  removeButton.onclick = () => {
+    ingredientContainer.remove();
+  };
+
+  const amount = document.createElement("input");
+  amount.className = "ingredient-amount";
+  amount.placeholder = "Amount";
+  amount.text = "text";
+
+  ingredientContainer.appendChild(ingredient);
+  ingredientContainer.appendChild(removeButton);
+  ingredientContainer.appendChild(amount);
+
+  ingredientsList.appendChild(ingredientContainer);
+};
